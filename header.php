ab1df9c44c3ac3f90dabf0bb293d8d085f0ec10d
tropicjam <jayyid99@gmail.com>
2021-04-08 20:35:45 +0100

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=devie-width, user-scalable=no initial-scale=0, minimum-scale=1.0">
-    <meta htpp-equiv="X-UA-Compatitible" content="ie-edge">
-    <title>James Davies</title>
-</head>
-<body>
